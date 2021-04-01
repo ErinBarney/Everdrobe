4f1f385e4bddfcfed462c1ff412ccbd82745ac2f
ErinBarney <68777283+ErinBarney@users.noreply.github.com>
2021-04-01 21:11:34 +0900

    toastを追加しました。

--- app/src/main/java/com/erin/Everdrobe/ui/shopping/ConfirmFragment.java
+++ app/src/main/java/com/erin/Everdrobe/ui/shopping/ConfirmFragment.java
@@ -2,10 +2,14 @@ package com.erin.Everdrobe.ui.shopping;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,6 +24,40 @@ public class ConfirmFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_confirm, container, false);
+
+
+        Button btnFragment4 = (Button) root.findViewById(R.id.button_no);
+        btnFragment4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+                fr.replace(R.id.container, new DetailFragment());
+                fr.commit();
+
+
+            }
+        });
+
+        Button btnFragment5 = (Button) root.findViewById(R.id.button_yes);
+
+        btnFragment5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+                fr.replace(R.id.container, new ShoppingFragment());
+                fr.commit();
+                Toast.makeText(getActivity(),"Borrowed!",Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+            }
+        });
+
 
 
 
